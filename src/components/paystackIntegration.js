d2964d847c44591fb2e4eadbf75a8eefd55c6a05
ikape <ikapeisaac3@gmail.com>
2023-05-21 19:47:34 +0100

    fixed some bugs

--- src/components/paystackIntegration.js
+++ src/components/paystackIntegration.js
@@ -1,72 +1,101 @@
-import { React, useState } from 'react'
+import { React, useState } from "react";
 // import PaystackPop from '@paystack/inline-js'
-import { usePaystackPayment } from 'react-paystack';
+import { usePaystackPayment } from "react-paystack";
 
 const PaystackIntegration = () => {
-    const [email, setEmail] = useState('')
-    const [amount, setAmount] = useState('')
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
 
-    const payWithPaystack = (e) => {
-        e.preventDefault()
-    const paystack = new  usePaystackPayment()
-        paystack.newTransaction({
-            key: "pk_test_ee97bfcd2f09ef39708d999978a685e9a0b55a1e",
-            amount: amount * 100,
-            email,
-            firstname,
-            lastname,
-            onSuccess(transaction) {
-                let message = `Payment Complete! Reference ${transaction.reference}`
-                alert(message)
-                setEmail("")
-                setAmount("")
-                setFirstname("")
-                setLastname("")
-            },
-            onCancel() {
-                alert("You have Canceled the transaction")
-            }
-        })
-    }
+  const config = {
+    reference: new Date().getTime().toString(),
+    email,
+    amount: amount * 100,
+    publicKey: "pk_test_ee97bfcd2f09ef39708d999978a685e9a0b55a1e"
+  };
 
-    return (
- <div className='form-container'>
-    <div className='form-heading'>
+  // you can call this function anything
+  const onSuccess = (reference) => {
+    // Implementation for whatever you want to do with reference and after success call.
+    console.log(reference);
+  };
+
+  // you can call this function anything
+  const onClose = () => {
+    // implementation for  whatever you want to do when the Paystack dialog closed.
+    console.log("closed");
+  };
+
+  const initializePayment = usePaystackPayment(config);
+
+  const payWithPaystack = (e) => {
+    e.preventDefault();
+
+    initializePayment(onSuccess, onClose);
+  };
+
+  return (
+    <div className='form-container'>
+      <div className='form-heading'>
         <h3 className='w3-center'>Make payment with Paystack</h3>
-    </div>
+      </div>
 
-       
-  <form id="paymentForm" className='form'>  
-                        {/* image             */}
-  <div class="form-group">
-    <label for="email">Email Address</label>
-    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className='email' id="email-address" required />
-  </div>
-                        
-  <div class="form-group">
-    <label for="amount">Amount</label>
-    <input type="tel" value={amount} onChange={(e)=>setAmount(e.target.value)} className='amount' id="amount" required />
-  </div>
-                        
-  <div class="form-group">
-    <label for="first-name">First Name</label>
-    <input type="text" value={firstname} onChange={(e)=>setFirstname(e.target.value)} className='firstname' id="first-name" />
-   </div>
-                        
-  <div class="form-group">
-    <label for="last-name">Last Name</label>
-    <input type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)} className='lastname' id="last-name" />
-  </div>
-                        
-   <div class="form-submit">
-    <button className='btn' type="submit" onclick={payWithPaystack}> Pay </button>
-   </div>   
-                        
-        </form>
-     
+      <form id='paymentForm' className='form' onSubmit={payWithPaystack}>
+        {/* image */}
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='email'
+            id='email-address'
+            required
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='amount'>Amount</label>
+          <input
+            type='tel'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className='amount'
+            id='amount'
+            required
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='first-name'>First Name</label>
+          <input
+            type='text'
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            className='firstname'
+            id='first-name'
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='last-name'>Last Name</label>
+          <input
+            type='text'
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            className='lastname'
+            id='last-name'
+          />
+        </div>
+
+        <div className='form-submit'>
+          <button type='submit' className='btn'>
+            Pay
+          </button>
+        </div>
+      </form>
     </div>
-    )
-}
-export default PaystackIntegration
+  );
+};
+export default PaystackIntegration;
